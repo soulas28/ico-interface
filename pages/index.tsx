0abe1c394e2a75f1f855f973d6e8fbdf6fe4907a
SoulaS <helloworldwithsoulas@gmail.com>
2022-02-05 16:33:15 +0900

    refactor: move CurrencyInput into CurrencyInput.tsx from index.html

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import { useState } from 'react'
 
 import { Button } from '../components/Button'
+import { CurrencyInput } from '../components/CurrencyInput'
 import { DownArrow } from '../components/DownArrow'
 import { Text } from '../components/Text'
 
@@ -115,51 +116,6 @@ const Home: NextPage = () => {
           />
         </div>
       </div>
-    </div>
-  )
-}
-
-interface CurrencyInputProps {
-  unit: 'ETH' | 'TKN'
-  hidden?: boolean
-  className?: string
-}
-
-const CurrencyInput: React.FC<CurrencyInputProps> = (props) => {
-  const [isBeingFocused, setIsBeingFocused] = useState(false)
-  return (
-    <div
-      className={
-        'flex w-[688px] flex-row items-center justify-evenly rounded-3xl bg-white-pink py-1.5 outline-[3.3px] drop-shadow hover:outline hover:outline-red-pink' +
-        ' ' +
-        (props.hidden ? 'invisible' : '') +
-        ' ' +
-        (props.className ? props.className : '') +
-        ' ' +
-        (isBeingFocused ? 'outline outline-red-pink' : '')
-      }
-    >
-      <div>
-        <Image
-          src="/eth-diamond.svg"
-          height="67"
-          width="41"
-          alt="eth diamond"
-          priority
-          className={props.unit === 'ETH' ? '' : 'invisible'}
-        />
-      </div>
-      <input
-        type="text"
-        className="max-w-[460px] grow bg-transparent font text-5xl text-blue-black/80 focus:outline-none"
-        defaultValue="437847323"
-        onFocus={() => setIsBeingFocused(true)}
-        onBlur={() => setIsBeingFocused(false)}
-      />
-      <Text
-        str={props.unit === 'ETH' ? 'ETH' : 'TKN'}
-        className="grow-0 text-5xl"
-      />
     </div>
   )
 }
